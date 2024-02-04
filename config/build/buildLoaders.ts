7dab36d0bd9a6b763e1fc7aaed66fc2bc6e1b503
Zu1930 <vladislav.ponomarenko9930@gmail.com>
2024-02-04 10:18:25 +0300

    initial commit

---
+++ config/build/buildLoaders.ts
@@ -0,0 +1,32 @@
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import webpack from 'webpack';
+import { BuildOptions } from './types/config';
+
+export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
+  // Ечли не используем тайпскрипт - нужен babel
+  const typescriptLoader = {
+    test: /\.tsx?$/,
+    use: 'ts-loader',
+    exclude: /node_modules/,
+  };
+
+  const cssLoader = {
+    test: /\.s[ac]ss$/i,
+    use: [
+      isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+      {
+        loader: 'css-loader',
+        options: {
+          modules: {
+            auto: (resPath: string) => Boolean(resPath.includes('.module.')),
+            localIdentName: isDev
+              ? '[path][bane]__[local]--[hash:base64:5]'
+              : '[hash:base64:5]',
+          },
+        },
+      },
+      'sass-loader',
+    ],
+  };
+  return [typescriptLoader, cssLoader];
+}
